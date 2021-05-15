543f9b790e9c2b6f651ddec02d8ccdd6a9b44de5
luidimso <luidi_mso@outlook.com>
2021-05-14 22:46:19 -0300

    Adding express

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 Install nodemon for development environment
 
 ```npm i -D nodemon@2.0.7```
+
+Install express
+
+```npm i -S express@4.17.1```
--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("Testing nodemon");
+import express from "express";
+
+const server = express();
+const PORT = 3000;
+
+server.listen(PORT, () => {
+    console.log("Server running on port "+PORT);
+});
