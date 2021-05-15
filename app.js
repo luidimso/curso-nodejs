aa9451d91a5207d3a215a6ba10aef35f9a9878e1
luidimso <luidi_mso@outlook.com>
2021-05-14 23:05:41 -0300

    Adding post, put and delete requisitions

--- app.js
+++ app.js
@@ -5,11 +5,24 @@ const server = express();
 const PORT = 3000;
 
 const createUrl = (version, path) => "/api/"+version+"/"+path;
+const BOOKS_URL = createUrl("v1", "books");
 
 server.listen(PORT, () => {
     console.log("Server running on port "+PORT);
 });
 
-server.get(createUrl("v1", "books"), (req, resp) => {
+server.get(BOOKS_URL, (req, resp) => {
     resp.json(books);
+});
+
+server.post(BOOKS_URL, (req, resp) => {
+    console.log("Testing")
+});
+
+server.put(BOOKS_URL, (req, resp) => {
+    console.log("Testing")
+});
+
+server.delete(BOOKS_URL, (req, resp) => {
+    console.log("Testing")
 });
