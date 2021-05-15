bfcd14ac836d04e08e1242fea5db08a4195796de
luidimso <luidi_mso@outlook.com>
2021-05-14 23:01:38 -0300

    Getting data from JSON file

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 import express from "express";
+import books from './data/books.json';
 
 const server = express();
 const PORT = 3000;
+
+const createUrl = (version, path) => "/api/"+version+"/"+path;
 
 server.listen(PORT, () => {
     console.log("Server running on port "+PORT);
 });
 
-server.get('/', (req, resp) => {
-    resp.send("Testing");
+server.get(createUrl("v1", "books"), (req, resp) => {
+    resp.json(books);
 });
